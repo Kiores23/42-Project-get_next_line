4020a66a47601465e07ed289dad3d82e03754540
Alexis Mery <amery@c1r2p3.42nice.fr>
2022-11-29 13:15:11 +0100

    first

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amery <amery@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 16:33:22 by amery             #+#    #+#             */
+/*   Updated: 2022/11/29 12:51:37 by amery            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_free_stackandbuff(char **stack, char *buffer);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *str);
+char	*ft_strchr(char *str, char c);
+void	ft_strlcpy(char *dst, const char *src, int size);
+
+#endif
